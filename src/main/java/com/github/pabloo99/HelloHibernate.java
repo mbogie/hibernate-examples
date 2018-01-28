6250f130d0d5809e3ce77c99ffd5894da88079c0
Paweł Mazur <pawel-mazur2@o2.pl>
2018-01-28 18:06:33 +0100

    Initial commit

---
+++ src/main/java/com/github/pabloo99/HelloHibernate.java
@@ -0,0 +1,10 @@
+package com.github.pabloo99;
+
+import org.apache.log4j.Logger;
+
+public class HelloHibernate {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(HelloHibernate.class);
+        logger.info("Hello JDBC!");
+    }
+}
