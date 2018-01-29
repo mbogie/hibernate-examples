34620d938c347917218041462fea2094dc6fb949
Paweł Mazur <pawel-mazur2@o2.pl>
2018-01-30 00:29:46 +0100

    Prepare empty project

--- src/main/java/com/github/pabloo99/entity/Employee.java
+++ src/main/java/com/github/pabloo99/entity/Employee.java
@@ -1,25 +1,4 @@
 package com.github.pabloo99.entity;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.sql.Date;
-
-@NoArgsConstructor
-@Data
 public class Employee {
-
-    private Integer id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phoneNumber;
-    private Date hireDate;
-    private String jobId;
-    private Double salary;
-    private Double commissionPct;
-    private Integer departmentId;
-    private Integer managerId;
-
-    private Department department;
 }
--- src/test/java/com/github/pabloo99/EmployeeDaoTest.java
+++ src/test/java/com/github/pabloo99/EmployeeDaoTest.java
@@ -1,24 +1,5 @@
 package com.github.pabloo99;
 
-import com.github.pabloo99.dao.EmployeeDao;
-import com.github.pabloo99.entity.Employee;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import java.util.List;
-
 public class EmployeeDaoTest {
 
-    private EmployeeDao employeeDao;
-
-    public EmployeeDaoTest() {
-        employeeDao = new EmployeeDao();
-    }
-
-    @Test
-    public void shouldFindAllEmployees(){
-        List<Employee> employeeList = employeeDao.findAll();
-
-        Assert.assertTrue(employeeList.size() > 0);
-    }
 }
